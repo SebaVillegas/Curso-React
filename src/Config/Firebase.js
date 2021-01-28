1a298e587379f4a917835ca4c7c6774f7a7e4120
Sebastian Villegas Vidal <svillegas74@gmail.com>
2021-01-27 22:59:40 -0300

    entrega realizada para el curso de React JS

---
+++ src/Config/Firebase.js
@@ -0,0 +1,28 @@
+import firebase from "firebase";
+import 'firebase/firestore';   // for cloud firestore
+
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCzwQtKRY9tYJpYgvNDTyUQ2LHtLpsmIZA",
+    authDomain: "curso-utn-90696.firebaseapp.com",
+    databaseURL: "https://curso-utn-90696-default-rtdb.firebaseio.com",
+    projectId: "curso-utn-90696",
+    storageBucket: "curso-utn-90696.appspot.com",
+    messagingSenderId: "292157745733",
+    appId: "1:292157745733:web:a23963c6c9f9ee4924912c",
+    measurementId: "G-HZV4WP3F0T"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const db = firebase.firestore();
+firebase.auth = firebase.auth();
+firebase.db = db;
+
+export default firebase;
+
+
+
+
